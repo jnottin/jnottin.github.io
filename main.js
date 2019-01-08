178e5dd2a2632ce4bc59c3e9f9baed7a0dcbb89f
Sam Nottingham <jnottin92@gmail.com>
2019-01-08 16:43:39 -0500

    finished up

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-const navContactIcons = document.querySelectorAll(".light-up");
+// const navContactIcons = document.querySelectorAll(".light-up");
 
 
-//SELECTING THE BUTTON
-const sayHelloBtn = document.querySelector(".sayHelloBtn");
+// //SELECTING THE BUTTON
+// const sayHelloBtn = document.querySelector(".sayHelloBtn");
 
-//ADDING EVENT LISTENER TO BUTTON
-sayHelloBtn.addEventListener("click", lightUpContactInfo);
-
-
-//FUNCTION RUN WHEN BUTTON CLICKED
-function lightUpContactInfo() {
-    console.log("function run")
-    navContactIcons.forEach(navIcon => {
-        if (navIcon) {
-            navIcon.style.boxShadow = "15px 10px 200px red inset";
-            navIcon.style.borderRadius = "15px";
-        } else {
-            navIcon.style.boxShadow = "none";
-        }
-        // navIcon.style.boxShadow = "5px 10px 20px red inset";
-        // navIcon.style.boxShadow = "0 0 200px #61dafb";
+// //ADDING EVENT LISTENER TO BUTTON
+// sayHelloBtn.addEventListener("click", lightUpContactInfo);
 
 
-    });
-}
+// //FUNCTION RUN WHEN BUTTON CLICKED
+// function lightUpContactInfo() {
+//     navContactIcons.forEach(navIcon => {
+//         if (navIcon.style.boxShadow = "none") {
+//             console.log("works")
+//             // navIcon.style.boxShadow = "0 0 200px #61dafb";
+//             // navIcon.style.borderRadius = "15px";
+//             // navIcon.style.height = "70px"
+//             // navIcon.style.width = "70px"
+//         } else {
+//             navIcon.style.boxShadow = "none";
+//         }
+//         // navIcon.style.boxShadow = "5px 10px 20px red inset";
+//         // navIcon.style.boxShadow = "0 0 200px #61dafb";
+
+
+//     });
+// }
